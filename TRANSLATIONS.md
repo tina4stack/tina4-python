d7bb96d5b7ed096d5c6bec91665982db765e2042
AbdulDavids <abdulbaaridavids04@gmail.com>
2023-11-13 18:16:45 +0200

    Added Localization Support

---
+++ TRANSLATIONS.md
@@ -0,0 +1,62 @@
+## Adding More Translations
+
+To add more translations to the project, follow these steps:
+
+### 1. Install Babel
+
+Ensure that the Babel library is installed. You can install it using the following command:
+
+```bash
+pip install Babel
+```
+
+### 2. Extract Messages
+
+Use the following command to extract messages from your Python files and generate a POT file:
+
+```bash
+pybabel extract -o messages.pot .
+```
+
+This will create a `messages.pot` file in the project directory.
+
+### 3. Initialize Translations
+
+Initialize translations for a new language. Replace `fr` with the language code of your choice:
+
+```bash
+pybabel init -i messages.pot -d translations -l fr
+```
+
+This will create a new directory `fr` under the `translations` directory with a `.po` file for the French language.
+
+### 4. Edit Translation Files
+
+Edit the generated `.po` file (`translations/fr/LC_MESSAGES/messages.po`) using a text editor. Translate the messages within the `msgstr` fields.
+
+### 5. Compile Translations
+
+After making translations, compile them into machine-readable `.mo` files:
+
+```bash
+pybabel compile -d translations
+```
+
+### 6. Update Translations
+
+If you make changes to the source code and need to update translations, use the following commands:
+
+```bash
+pybabel extract -F babel.cfg -o messages.pot .
+pybabel update -i messages.pot -d translations
+```
+
+### 7. Run the Application in a Different Language
+
+Run your Tina4 Python application with the desired language. For example, to run it in French:
+
+```bash
+poetry run main.py fr
+```
+
+Replace `fr` with the language code you've added translations for.
--- tina4_python/__init__.py
+++ tina4_python/__init__.py
@@ -1,30 +1,57 @@
-#
-# Tina4 - This is not a 4ramework.
-# Copy-right 2007 - current Tina4
-# License: MIT https://opensource.org/licenses/MIT
-#
-__version__ = '0.1.0'
-
-from tina4_python.Env import load_env
-from tina4_python.Webserver import Webserver
-from tina4_python.Router import Router, response, get
-import importlib
-import sys
+import gettext
 import os
 import shutil
+import importlib
+import sys
+from tina4_python.Env import load_env
+from tina4_python.Webserver import Webserver
+from tina4_python.Router import Router
+
+# Localization setup
+translation_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'translations')
+
+available_languages = ['en', 'fr', 'af']
+user_language = 'en'
+
+# check if argument is a language
+if len(sys.argv) > 1:
+    try:
+        int(sys.argv[1])
+    except ValueError:
+        if sys.argv[1] in available_languages:
+            user_language = sys.argv[1]
+
+if len(sys.argv) > 2 and sys.argv[2] in available_languages:
+    user_language = sys.argv[2]
+
+# Initialize the translation system
+translation = gettext.translation('messages', translation_path, languages=[user_language])
+translation.install()
+
+# translations, add more languages here
+# TODO: Need to make separate core file for messages
+MSG_ASSUMING_ROOT_PATH = _('Assuming root path: {root_path}, library path: {library_path}')
+MSG_LOAD_ALL_THINGS = _('Load all things')
+MSG_SERVER_STARTED = _('Server started http://{host_name}:{port}')
+MSG_SERVER_STOPPED = _('Server stopped.')
+MSG_STARTING_WEBSERVER = _('Starting webserver on {port}')
+MSG_ENTRY_POINT_NAME = _('Entry point name ... {name}')
+
 
 if importlib.util.find_spec("jurigged"):
     import jurigged
 
-# define the variable to be used for global routes
+# Define the variable to be used for global routes
+
 tina4_routes = []
 
 library_path = os.path.dirname(os.path.realpath(__file__))
 
 root_path = os.path.realpath(os.getcwd())
-print("Assuming root path:", root_path, "library path:", library_path)
 
-# hack for local development
+print(MSG_ASSUMING_ROOT_PATH.format(root_path=root_path, library_path=library_path))
+
+# Hack for local development
 if root_path.count("tina4_python") > 0:
     root_path = root_path.split("tina4_python")[0][:-1]
 
@@ -46,15 +73,13 @@ if not os.path.exists(root_path + os.sep + "src"):
 
 from src import *
 
-
 def initialize():
-    print("Load all things")
-
+    print(MSG_LOAD_ALL_THINGS)
 
 def webserver(host_name, port):
-    web_server = Webserver(host_name, int(port)) #HTTPServer((host_name, int(port)), Webserver)
+    web_server = Webserver(host_name, int(port))  # HTTPServer((host_name, int(port)), Webserver)
     web_server.router_handler = Router()
-    print("Server started http://%s:%s" % (host_name, port))
+    print(MSG_SERVER_STARTED.format(host_name=host_name, port=port))
 
     try:
         web_server.serve_forever()
@@ -62,29 +87,29 @@ def webserver(host_name, port):
         pass
 
     web_server.server_close()
-    print("Server stopped.")
+    print(MSG_SERVER_STOPPED)
 
-
-def main(in_hostname="localhost",in_port=7145):
-    print("Starting webserver on", in_port)
+def main(in_hostname="localhost", in_port=7145):
+    print(MSG_STARTING_WEBSERVER.format(port=in_port))
     load_env()
     initialize()
     webserver(in_hostname, in_port)
 
-
 if importlib.util.find_spec("jurigged"):
     jurigged.watch("./src")
 
-print("Entry point name ...", __name__)
+print(MSG_ENTRY_POINT_NAME.format(name=__name__))
 if __name__ == '__main__' or __name__ == 'tina4_python':
     # Start up a webserver based on params passed on the command line
     HOSTNAME = "localhost"
     PORT = 7145
     if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        PORT = sys.argv[1]
-        if ":" in PORT:
-            SERVER_CONFIG = PORT.split(":")
-            HOSTNAME = SERVER_CONFIG[0]
-            PORT = SERVER_CONFIG[1]
+        PORT = int(sys.argv[1])
+        if len(sys.argv) > 2 and sys.argv[2].isdigit():
+            PORT = int(sys.argv[2])
+            if ":" in PORT:
+                SERVER_CONFIG = PORT.split(":")
+                HOSTNAME = SERVER_CONFIG[0]
+                PORT = SERVER_CONFIG[1]
 
     main(HOSTNAME, PORT)
