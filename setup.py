7a0439bcb15cb63221e404d1368f030bbd5d06a3
Andre van Zuydam <andrevanzuydam@gmail.com>
2024-03-23 15:53:26 +0200

    Added setup.py for pip description

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages, setup
 setup(
     name="tina4_python",
     version="0.1.32",
-    description="A placeholder for future Ultralytics projects.",
-    long_description=Path("README_simple.md").read_text(encoding="utf-8"),
+    description="Tina4Python - This is not a framework for Python",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     author="Andre van Zuydam",
     author_email="andrevanzuydam@gmail.com",
