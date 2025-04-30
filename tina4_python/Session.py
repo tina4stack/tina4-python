01fbc61e4e431155abb15739b2c3e00c10b5ca54
Jacques van Zuydam <jtvanzuydam@gmail.com>
2025-04-30 14:25:42 +0200

    Update Session.py to handle Strict Redis
    If connecting to an external secure Redis with authentication in place the StrictRedis method is required with the password parameter.

--- tina4_python/Session.py
+++ tina4_python/Session.py
@@ -111,7 +111,10 @@ class SessionRedisHandler(SessionHandler):
             Debug("Redis not installed, install with pip install redis or poetry add redis", str(e), Constant.TINA4_LOG_ERROR)
             sys.exit(1)
 
-        redis_instance = redis.Redis(host=os.getenv("TINA4_SESSION_REDIS_HOST", "localhost"), port=os.getenv("TINA4_SESSION_REDIS_PORT",6379), decode_responses=True)
+        if os.getenv('TINA_4_SESSION_REDIS_PASSWORD'):
+            redis_instance = redis.StrictRedis(host=os.getenv("TINA4_SESSION_REDIS_HOST", "localhost"), port=os.getenv("TINA4_SESSION_REDIS_PORT",6379), password=os.getenv('TINA_4_SESSION_REDIS_PASSWORD'), decode_responses=True)
+        else:
+            redis_instance = redis.Redis(host=os.getenv("TINA4_SESSION_REDIS_HOST", "localhost"), port=os.getenv("TINA4_SESSION_REDIS_PORT",6379), decode_responses=True)
 
         return redis_instance
 
