bbe14d7210a1d5c9a5eb715c92ee3fc77339a81d
Andre van Zuydam <andrevanzuydam@gmail.com>
2025-07-05 21:15:16 +0200

    Release v0.2.100 Fixes for queue

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
-rm dist/*.whl
-rm dist/*.tar.gz
+del dist/*.whl
+del dist/*.tar.gz
 uv export --format requirements-txt
 uv build --no-sources
-uv publish --username __token__ --password $UV_PYPI_PASSWORD
+uv publish --username __token__ --password %UV_PYPI_PASSWORD%
